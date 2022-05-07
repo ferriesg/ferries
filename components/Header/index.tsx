931eaca68288b258b7091d4f8781958251a92f34
weiming <ferries_g@163.com>
2022-05-07 18:07:23 +0800

    change Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <div style={{ backgroundColor: "#000", height: "50px" }}>
+      <div
+        style={{
+          width: "1024px",
+          color: "#FFF",
+          fontWeight: "700",
+          fontSize: "24px",
+          margin: "0 auto",
+          lineHeight: "50px",
+          fontStyle: "italic",
+        }}
+      >
+        Ferries
+      </div>
+    </div>
+  );
+}
+export default Header;
